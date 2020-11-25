52c5c1303f002fcd042382eacf8acac01a79e615
Fabian Hepke <fabian.hepke@stp-online.de>
2020-11-25 16:45:02 +0100

    Button to come back to the profile page

--- app/src/main/java/com/example/memevz/EditProfileActivity.java
+++ app/src/main/java/com/example/memevz/EditProfileActivity.java
@@ -1,18 +1,36 @@
 package com.example.memevz;
 
-import androidx.appcompat.app.AppCompatActivity;
-
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageButton;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 public class EditProfileActivity extends AppCompatActivity {
 
     private User user;
     private String eMail, username;
-
+    private ImageButton back;
+    private Button saveChanges;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_edit_profile);
+        back = (ImageButton)findViewById(R.id.btn_back_to_profile);
+        saveChanges = (Button)findViewById(R.id.btn_save_changes);
+        back.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openProfile();
+            }
+        });
+    }
+    private void openProfile() {
+        Intent intent = new Intent(this, ProfileActivity.class);
+        startActivity(intent);
+        overridePendingTransition(R.anim.slide_in_left, R.anim.slide_out_right);
     }
 }
