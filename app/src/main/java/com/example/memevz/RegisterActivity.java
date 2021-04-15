011cac541002b22fed759ed79ba008deb04445fb
Fabian Hepke <fabian.hepke@stp-online.de>
2021-04-15 09:42:39 +0200

    add new user to database

--- app/src/main/java/com/example/memevz/RegisterActivity.java
+++ app/src/main/java/com/example/memevz/RegisterActivity.java
@@ -11,6 +11,9 @@ import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.database.RoomDB;
+import com.database.UserDB;
+import com.google.android.material.textfield.TextInputEditText;
 import com.google.android.material.textfield.TextInputLayout;
 
 public class RegisterActivity extends AppCompatActivity {
@@ -28,7 +31,8 @@ public class RegisterActivity extends AppCompatActivity {
         btn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                openHomeActivity();
+                //openHomeActivity();
+                register();
             }
         });
         signIn.setOnClickListener(new View.OnClickListener() {
@@ -39,8 +43,29 @@ public class RegisterActivity extends AppCompatActivity {
         });
     }
 
+    private void register() {
+        UserDB user = new UserDB();
+        TextInputEditText mail = findViewById(R.id.email);
+        TextInputEditText username = findViewById(R.id.username);
+        TextInputEditText password1 = findViewById(R.id.password);
+        TextInputEditText password2 = findViewById(R.id.password2);
+        if (mail.getText().toString() != "") {
+            user.setMail(mail.getText().toString());
+        }
+        if (username.getText().toString() != "") {
+            user.setUsername(username.getText().toString());
+        }
+        if ((password1.getText().toString() != "") && (password1.getText().toString().equals(password2.getText().toString()))) {
+            user.setPassword(password1.getText().toString());
+        }
+
+        RoomDB database = RoomDB.getInstance(this);
+        database.userDao().insert(user);
+
+    }
+
     private void openSignInActivity() {
-        Intent intent = new Intent(RegisterActivity.this, MainActivity.class);
+        Intent intent = new Intent(RegisterActivity.this, LoginActivity.class);
         TextView slogan = findViewById(R.id.slogan);
         Button login = findViewById(R.id.login);
         ImageView logo = findViewById(R.id.logo);
