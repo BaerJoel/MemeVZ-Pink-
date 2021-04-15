8c5eb235eaffaabb722f70477d25db0bf69d10ed
Fabian Hepke <fabian.hepke@stp-online.de>
2021-04-15 15:15:13 +0200

    delete a meme

--- app/src/main/java/com/database/MemeDao.java
+++ app/src/main/java/com/database/MemeDao.java
@@ -1,6 +1,7 @@
 package com.database;
 
 import androidx.room.Dao;
+import androidx.room.Delete;
 import androidx.room.Insert;
 import androidx.room.Query;
 import androidx.room.Update;
@@ -18,8 +19,11 @@ public interface MemeDao {
     @Query("SELECT (sum(likes) - sum(dislikes)) as score FROM meme WHERE uploader = :user_id")
     Long getUserScore(Long user_id);
 
-    @Query("SELECT * FROM meme WHERE uploader = :user_id")
+    @Query("SELECT * FROM meme WHERE uploader = :user_id ORDER BY id DESC")
     List<MemeDB> getAllMemesFromUser(Long user_id);
+
+    @Delete
+    void deleteMeme(MemeDB meme);
 
     @Update
     void updateMeme(MemeDB meme);
