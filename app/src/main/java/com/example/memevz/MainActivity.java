b9e105765b6aac0e3a5d3d91d846317ef918cc3a
Fabian Hepke <fabian.hepke@stp-online.de>
2021-04-15 09:43:34 +0200

    loading activity

--- app/src/main/java/com/example/memevz/MainActivity.java
+++ app/src/main/java/com/example/memevz/MainActivity.java
@@ -1,67 +1,38 @@
 package com.example.memevz;
 
-import android.app.ActivityOptions;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
-import android.util.Pair;
-import android.view.View;
-import android.widget.Button;
-import android.widget.ImageView;
-import android.widget.TextView;
+import android.os.Handler;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import com.google.android.material.textfield.TextInputLayout;
-
 public class MainActivity extends AppCompatActivity {
-private Button btn;
-private TextView register, usernameEditText, passwordEditText;
-private User user;
-private StartUp startUp;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        btn = (Button) findViewById(R.id.login);
-        register = (TextView) findViewById(R.id.change);
-        btn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                openHomeActivity();
-            }
-        });
-        register.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                openRegisterActivity();
-            }
-        });
-
+        SharedPreferences s = getSharedPreferences("User", MODE_PRIVATE);
+        if(s.getBoolean("isLoggedIn", false)) {
+            final Intent intent = new Intent(MainActivity.this, HomeActivity.class);
+            Handler handler = new Handler();
+            handler.postDelayed(new Runnable() {
+                @Override
+                public void run() {
+                    startActivity(intent);
+                }
+            }, 300);
+        }
+        else {
+            final Intent intent = new Intent(MainActivity.this, LoginActivity.class);
+            Handler handler = new Handler();
+            handler.postDelayed(new Runnable() {
+                @Override
+                public void run() {
+                    startActivity(intent);
+                }
+            }, 300);
+        }
     }
-
-    private void openRegisterActivity() {
-        Intent intent = new Intent(MainActivity.this, RegisterActivity.class);
-        TextView slogan = findViewById(R.id.slogan);
-        ImageView logo = findViewById(R.id.login_logo);
-        Button login = findViewById(R.id.login);
-        TextInputLayout password = findViewById(R.id.password_layout);
-        TextInputLayout mail = findViewById(R.id.username_layout);
-
-        Pair[] pairs = new Pair[5];
-        pairs[0] = new Pair<View, String>((View) login, "login");
-        pairs[1] = new Pair<View, String>((View) slogan, "slogan");
-        pairs[2] = new Pair<View, String>((View) logo, "logo");
-        pairs[3] = new Pair<View, String>((View) password, "password");
-        pairs[4] = new Pair<View, String>((View) mail, "mail");
-        ActivityOptions options = ActivityOptions.makeSceneTransitionAnimation(MainActivity.this, pairs);
-        startActivity(intent, options.toBundle());
-    }
-
-    private void openHomeActivity() {
-        Intent intent = new Intent(this, HomeActivity.class);
-        startActivity(intent);
-        overridePendingTransition(R.anim.slide_in_right, R.anim.slide_out_left);
-    }
-
 }
