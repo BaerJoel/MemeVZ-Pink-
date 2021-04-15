6cb6f9bebb591803a7332b09c36afdc9a4f541d5
Fabian Hepke <fabian.hepke@stp-online.de>
2021-04-15 14:35:52 +0200

    some usefull querys

--- app/src/main/java/com/database/MemeDao.java
+++ app/src/main/java/com/database/MemeDao.java
@@ -3,6 +3,7 @@ package com.database;
 import androidx.room.Dao;
 import androidx.room.Insert;
 import androidx.room.Query;
+import androidx.room.Update;
 
 import java.util.List;
 
@@ -13,4 +14,13 @@ public interface MemeDao {
 
     @Query("SELECT * FROM meme")
     List<MemeDB> getAllMemes();
+
+    @Query("SELECT (sum(likes) - sum(dislikes)) as score FROM meme WHERE uploader = :user_id")
+    Long getUserScore(Long user_id);
+
+    @Query("SELECT * FROM meme WHERE uploader = :user_id")
+    List<MemeDB> getAllMemesFromUser(Long user_id);
+
+    @Update
+    void updateMeme(MemeDB meme);
 }
